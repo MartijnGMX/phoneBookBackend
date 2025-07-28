d791b493f1fbf7bd323e5eebce479649a0a77d5c
Martijn Kragtwijk <martijn@pwxr.world>
2025-07-28 23:09:34 +0200

    changed port nr for Render service

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ app.post('/api/persons', (request, response) => {
 
 app.use(unknownEndPoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`Phonebook backend running on port ${PORT} `)
